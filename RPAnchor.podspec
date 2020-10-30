df2650bc1e78074399c996eaddf1f0e919d2a12c
dengfeng520 <deng_feng520@163.com>
2020-10-30 10:05:23 +0800

    update code

--- RPAnchor.podspec
+++ RPAnchor.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |spec|
   #
 
   spec.name         = "RPAnchor"
-  spec.version      = "1.1"
+  spec.version      = "1.2"
   spec.summary      = "'RPAnchor' is a chain programming library."
 
   # This description is used to generate tags and improve search results.
---
+++ RPAnchor/RPLayoutAnchor/RPAnchor.h
@@ -0,0 +1,16 @@
+//
+//  RPAnchor.h
+//  RPAnchor
+//
+//  Created by rp.wang on 2020/10/30.
+//  Copyright © 2020 西安博信信息科技有限公司. All rights reserved.
+//
+
+#ifndef RPAnchor_h
+#define RPAnchor_h
+
+#import "UIView+Fetch.h"
+#import "UIView+Anchor.h"
+#import "UIView+UpdateLayout.h"
+
+#endif /* RPAnchor_h */
